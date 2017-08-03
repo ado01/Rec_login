caee7ebbb4facd632446ebd399105a49f4b03e82
caluisi <adolfocaluisi@libero.it>
2017-08-03 10:49:44 +0200

    only BurpExtender

--- src/burp/BurpExtender.java
+++ src/burp/BurpExtender.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package burp;
 
 import java.io.PrintWriter;
@@ -10,7 +6,7 @@ import rec_login.Rec_Login;
 
 /**
  *
- * @author caluisi
+ * @author xyzt
  */
 public class BurpExtender implements IBurpExtender{
 
