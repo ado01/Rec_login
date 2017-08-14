103a2d9732e077d76572d28fccccef193969cd77
caluisi <adolfocaluisi@libero.it>
2017-08-14 12:09:59 +0200

    modifica Msg_Request

--- src/rec_login/Msg_Request.java
+++ src/rec_login/Msg_Request.java
@@ -1,14 +1,52 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package rec_login;
+
+import burp.IBurpExtenderCallbacks;
+import burp.IExtensionHelpers;
+import burp.IHttpRequestResponse;
+import java.io.PrintWriter;
 
 /**
  *
  * @author xyzt
  */
-public class Msg_Request {
+public class Msg_Request implements Runnable {
+
+    private PrintWriter Sout; 
+    private IBurpExtenderCallbacks callbacks;
+    private IHttpRequestResponse req;
+    private IExtensionHelpers helper;
+    public IHttpRequestResponse lastresponse;
+    public int flagLastReq;
+    
+    public Msg_Request(IBurpExtenderCallbacks callbacks, PrintWriter Sout, IHttpRequestResponse req, IHttpRequestResponse lastresponse,int flagLastReq){
+        this.Sout = Sout;
+        this.callbacks = callbacks;
+        this.req = req;
+        this.lastresponse = lastresponse;
+        this.flagLastReq = flagLastReq;
+    }
+    
+    @Override
+    public void run() {
+        try{
+            if(flagLastReq == 0){
+                lastresponse = callbacks.makeHttpRequest(req.getHttpService(), req.getRequest());
+                //Sout.println(helper.bytesToString(response.getResponse()));
+            }else {
+                lastresponse = callbacks.makeHttpRequest(req.getHttpService(), req.getRequest());
+                Sout.println("ULTIMA RICHIESTA");
+            }
+        }catch(Exception e){
+            Sout.println("Generata un eccezione all'interno del thread");
+            //System.exit(0);
+            Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+                @Override
+                public void uncaughtException(Thread t, Throwable e) {
+                    Sout.println("Generata un eccezione all'interno del thread");
+                }
+            });
+        }
+    }
     
 }
